cb0578c183b0ec0f1a0ca2ed9edb18b73dfe1e30
Frank Henard <fhena2@illinois.edu>
2019-02-05 13:29:59 -0600

    remove getting-started text and pics from home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -120,7 +120,7 @@ export default class HomeScreen extends React.Component {
         return (
           <View style={styles.container}>
             <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-              <View style={styles.welcomeContainer}>
+                {/*<View style={styles.welcomeContainer}>
                 <Image
                   source={
                       __DEV__
@@ -129,34 +129,13 @@ export default class HomeScreen extends React.Component {
                   }
                   style={styles.welcomeImage}
                 />
-              </View>
+                </View>*/}
 
               <View style={styles.getStartedContainer}>
                 {/*this._maybeRenderDevelopmentModeWarning()*/}
-
                 { dynoButtons }
-
-
-                <Text style={styles.getStartedText}>Hello Campus Initiative Platform Team</Text>
-
-                <Text>--------------------------------------------</Text>
-
-                <Text style={styles.getStartedText}>Get started by opening</Text>
-
-                <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-                  <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-                </View>
-
-                <Text style={styles.getStartedText}>
-                  Change this text and your app will automatically reload.
-                </Text>
               </View>
 
-              <View style={styles.helpContainer}>
-                <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-                  <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-                </TouchableOpacity>
-              </View>
             </ScrollView>
 
             <View style={styles.tabBarInfoContainer}>
